8aeb16af5999d67549924a408fdf7440de163bad
pawan-bobbili <pawanbob007@gmail.com>
2020-08-05 08:18:25 +0530

    bugs fixed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,15 +7,15 @@ import { Route } from "react-router-dom";
 import Start from "./containers/Start/Start";
 
 class App extends React.Component {
-  render() {
-    return (
-      <Layout>
-        <Route path="/" exact component={Auth} />
-        <Route path="/start" exact component={Start} />
-        <Route path="/game" exact component={Game} />
-      </Layout>
-    );
-  }
+    render() {
+        return (
+            <Layout>
+                <Route path="/" exact component={Auth} />
+                <Route path="/start" exact component={Start} />
+                <Route path="/game" exact component={Game} />
+            </Layout>
+        );
+    }
 }
 
 export default App;
