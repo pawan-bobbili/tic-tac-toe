5ce3a2d76790bb7c2c18dad2770df607e064f776
pawan-bobbili <pawanbob007@gmail.com>
2020-08-08 21:20:53 +0530

    Added New Options

--- frontend/src/components/UI/Toolbar/Toolbar.js
+++ frontend/src/components/UI/Toolbar/Toolbar.js
@@ -1,9 +1,28 @@
+import { connect } from "react-redux";
 import React from "react";
 
 import styles from "./Toolbar.module.css";
 
 const Toolbar = (props) => {
-  return <div className={styles.Toolbar}>Tic-Tac-Toe</div>;
+    return (
+        <div className={styles.Toolbar}>
+            <div className={styles.Title}>Tic-Tac-Toe</div>
+            {props.isAuth ? (
+                <div className={styles.Nav}>
+                    <div>My Account</div>
+                    <div>Sign Out</div>
+                </div>
+            ) : (
+                <div className={styles.About}>About Us</div>
+            )}
+        </div>
+    );
 };
 
-export default Toolbar;
+const mapStateToProps = (state) => {
+    return {
+        isAuth: state.auth.authenticated,
+    };
+};
+
+export default connect(mapStateToProps)(Toolbar);
